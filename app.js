ef2e3957f416ccd895010111ac036cf64e0b2a59
Tootin0730 <yisan1004@gmail.com>
2022-12-21 17:40:51 -0500

    #2.4 Booleans

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const a = 9;
-const b = 3;
-let myName = "San";
-
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log("Hello " + myName);
-
-myName = "San Yi";
-
-console.log("Your new name is " + myName);
+const amIFat = null;
+let something;
+console.log(something, amIFat);
