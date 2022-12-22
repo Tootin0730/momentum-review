5e54f86f485b1a1d06b915e1dc69e557e7457073
Tootin0730 <yisan1004@gmail.com>
2022-12-22 18:50:49 -0500

    #3.3 Events

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const title = document.querySelector(".hello h1");
 
-title.innerText = "Hello";
+function handleTitleClick() {
+  console.log("Title was clicked!");
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick);
