35b7e75a33cc3c7de9ba5eb6533c84b6b3019db1
Tootin0730 <yisan1004@gmail.com>
2022-12-22 16:49:38 -0500

    #2.11 Returns

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const calculator = {
-  add: function (a, b) {
-    console.log(a, b);
+  plus: function (a, b) {
+    return a + b;
+  },
+  minus: function (a, b) {
+    return a - b;
+  },
+  times: function (a, b) {
+    return a / b;
+  },
+  divide: function (a, b) {
+    return a * b;
+  },
+  power: function (a, b) {
+    return a ** b;
   },
 };
 
-calculator.add(5, 1);
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.power(divideResult, minusResult);
