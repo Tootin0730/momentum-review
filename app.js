a59284562e5f5e9a69babeed121b50877c4098a6
Tootin0730 <yisan1004@gmail.com>
2022-12-22 17:40:44 -0500

    #2.16 Recap

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const age = parseInt(prompt("How old are you?"));
 
-if (isNaN(age)) {
+if (isNaN(age) || age < 0) {
   console.log("Please write a number.");
 } else if (age < 18) {
   console.log("You are too young.");
@@ -8,6 +8,8 @@ if (isNaN(age)) {
   console.log("You can drink.");
 } else if (age > 50 && age <= 80) {
   console.log("You must take care of your health.");
+} else if (age == 100) {
+  console.log("Wow you are wise!");
 } else if (age > 80) {
   console.log("You can do whatever you want.");
 }
