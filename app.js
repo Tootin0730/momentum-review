b3dfa0a2f231251b987770c2b488e5a44dfbe5de
Tootin0730 <yisan1004@gmail.com>
2022-12-23 17:29:41 -0500

    #4.3 Events pt.2

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
-  tomato.preventDefault();
+  event.preventDefault();
   console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+  console.log(event);
+  alert("clicked");
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
