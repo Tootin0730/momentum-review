3d7a0c254c1c09a456fa0e3e74bd771802b5203e
Tootin0730 <yisan1004@gmail.com>
2022-12-24 19:14:43 -0500

    #7.8 Deleting To Dos pt.3

--- js/todo.js
+++ js/todo.js
@@ -12,8 +12,9 @@ function saveTodos() {
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
-  console.log(li.id);
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveTodos();
 }
 
 function paintToDo(newTodo) {
